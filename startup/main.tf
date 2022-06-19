84f72358878620d2b1cd1cd634bf4c30b6993860
jawadyousoufi <javadyousefi21@gmail.com>
2022-06-19 16:46:13 +0000

    added readme

--- startup/main.tf
+++ startup/main.tf
@@ -5,15 +5,12 @@ resource "aws_vpc" "main" {
   enable_dns_support   = true
   tags                 = var.tags
 }
-
-
 resource "aws_subnet" "public1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet1
   tags                    = var.tags
   map_public_ip_on_launch = true
 }
-
 resource "aws_subnet" "public2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet2
@@ -26,14 +23,12 @@ resource "aws_subnet" "public3" {
   tags                    = var.tags
   map_public_ip_on_launch = true
 }
-
 resource "aws_subnet" "private1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.private_subnet1
   tags                    = var.tags
   map_public_ip_on_launch = false
 }
-
 resource "aws_subnet" "private2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.private_subnet2
@@ -46,13 +41,11 @@ resource "aws_subnet" "private3" {
   tags                    = var.tags
   map_public_ip_on_launch = false
 }
-
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
   tags   = var.tags
 
 }
-
 resource "aws_route_table" "example" {
   vpc_id = aws_vpc.main.id
 
@@ -62,7 +55,6 @@ resource "aws_route_table" "example" {
   }
   tags = var.tags
 }
-
 resource "aws_route_table_association" "public1" {
   subnet_id      = aws_subnet.public1.id
   route_table_id = aws_route_table.example.id
@@ -75,4 +67,40 @@ resource "aws_route_table_association" "public3" {
   subnet_id      = aws_subnet.public3.id
   route_table_id = aws_route_table.example.id
 }
+# resource "aws_route_table" "private" {
+#   vpc_id = aws_vpc.main.id
+
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_nat_gateway.example.id
+#   }
+#   tags = var.tags
+# }
+
+
+# resource "aws_eip" "nat" {
+#   vpc = true
+# }
+# resource "aws_nat_gateway" "example" {
+#   allocation_id = aws_eip.nat.id
+#   subnet_id     = aws_subnet.public1.id
+#   depends_on    = ["aws_internet_gateway.gw"]
+#   tags          = var.tags
+
+
+# }
+
+# resource "aws_route_table_association" "private1" {
+#   subnet_id      = aws_subnet.private1.id
+#   route_table_id = aws_route_table.private.id
+# }
+# resource "aws_route_table_association" "private2" {
+#   subnet_id      = aws_subnet.private2.id
+#   route_table_id = aws_route_table.private.id
+# }
+# resource "aws_route_table_association" "private3" {
+#   subnet_id      = aws_subnet.private3.id
+#   route_table_id = aws_route_table.private.id
+# }
+
 
--- startup/module/main.tf
+++ startup/module/main.tf
@@ -1,6 +1,3 @@
-### Instruction how to build VPC
-### Please copy paste below code
-```
 module "vpc" {
     source = "../"
     cidr_block      = "10.0.0.0/16"
@@ -17,11 +14,6 @@ module "vpc" {
     }    
 }
 
-```
-
-
-### To get the output, please add the following code
-```
 output vpc_id {
     value = module.vpc.vpc_id
 }
@@ -35,4 +27,3 @@ output public_subnet2 {
 output public_subnet3 {
     value = module.vpc.public_subnet3
 }
-```
