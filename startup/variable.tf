dbf46361b3938232211e292c249ce1f2999bf1f4
jwadyousoufi <javadyousefi21@gmail.com>
2022-06-19 00:39:57 +0000

    added

---
+++ startup/variable.tf
@@ -0,0 +1,11 @@
+variable "cidr_block" {
+  description = "Please provide CIDR block for VPC"
+  type        = string
+  default     = ""
+}
+
+variable "region" {
+  description = " Please provide a region name"
+  type        = string
+  default     = ""
+}
